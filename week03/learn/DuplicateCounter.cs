838c37023e3b36c1aaff1fc18156f3cee5010248
jeffrey-thomas <jeffrey.craig.thomas@gmail.com>
2024-10-03 11:04:08 -0700

    week03 learning activity

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -1,4 +1,6 @@
-﻿public class DuplicateCounter
+﻿using System.Collections.Generic;
+
+public class DuplicateCounter
 {
     //Count how many duplicates are in a collection of data.
 
@@ -24,7 +26,7 @@
 
     private static int CountDuplicates(int[] data)
     {
-        // Add code here.
-        return 0;
+        var set = new HashSet<int>(data);
+        return data.Length - set.Count;
     }
 }
